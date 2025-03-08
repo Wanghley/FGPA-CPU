442e0dc15dde5b2bf09711d1e00772a0adad9c4f
Wanghley Soares Martins <wanghleys@gmail.com>
2025-03-07 22:43:40 -0500

    feat: enhance exception handling for ALU and multidiv operations

--- main/proc/exception.v
+++ main/proc/exception.v
@@ -1,25 +1,26 @@
 module exception(
     opcode,
-    aluop,
+    dx_opcode,
     alu_ovf,
     multidiv_exception,
     exception
 );
 
-input [4:0] opcode, aluop;
+input [4:0] opcode, dx_opcode;
 input alu_ovf, multidiv_exception;
 output [31:0] exception;
 
 wire is_alu_exception;
-assign is_alu_exception = ((opcode == 5'b00000) || (opcode == 5'b00101)) && alu_ovf;
+assign is_alu_exception = ((opcode == 5'b00000) || (opcode == 5'b00101)) && (alu_ovf || multidiv_exception);
 
 wire [31:0] alu_exception;
 assign alu_exception = is_alu_exception ? 
                        (opcode == 5'b00101 ? 32'd2 :     // ADDI overflow
-                       (aluop == 5'b00000 ? 32'd1 :      // ADD overflow
-                       (aluop == 5'b00001 ? 32'd3 :      // SUB overflow
-                       32'd0)))                          // Default case
-                       : 32'd0;
+                       (dx_opcode == 5'b00000 ? 32'd1 :      // ADD overflow
+                       (dx_opcode == 5'b00001 ? 32'd3 :      // SUB overflow
+                       (dx_opcode == 5'b00110 ? 32'd4 :      // MUL overflow
+                       (dx_opcode == 5'b00111 ? 32'd5 :      // DIV overflow
+                          32'd0))))) : 32'd0;
 
 // TODO: implement multiplication and division exceptions
 assign exception = alu_exception;
