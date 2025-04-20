f4771e6d29479e7e3c098a45a62365e29ee11845
Wanghley Soares Martins <wanghleys@gmail.com>
2025-04-20 11:44:58 -0400

    fix: correct memory initialization and update load instruction in emg-test

--- assembler-python-version/code/emg-test.s
+++ assembler-python-version/code/emg-test.s
@@ -1,4 +1,4 @@
 loop:
-    addi $8, $8, 1 # Increment register $t0 by 1
-    # lw $8, 3199($zero) # Load word from memory address 0x00000FF0 into register $t0
+    # addi $8, $8, 1 # Increment register $t0 by 1
+    lw $9, 2049($zero) # Load word from memory address 0x00000FF0 into register $t0
     j loop # Jump to the label 'loop'
