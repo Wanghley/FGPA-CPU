93b9c73692442887e47c387bc9e2051ec6407779
Wanghley Soares Martins <wanghleys@gmail.com>
2025-04-20 15:03:31 -0400

    feat: include R-only port for VGA

--- main/proc/RAM.v
+++ main/proc/RAM.v
@@ -2,16 +2,22 @@
 module RAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 4096) (
     input wire                     clk,
 
-    // CPU Side (Port A - Original naming)
+    // CPU Side (Port A - RW)
     input wire                     wEn,
     input wire [ADDRESS_WIDTH-1:0] addr,
     input wire [DATA_WIDTH-1:0]    dataIn,
     output reg [DATA_WIDTH-1:0]    dataOut = 0,
 
-    // ADC Side (Port B - New)
+    // ADC Side (Port B - W)
+    // Note: This port is write-only, no readback
     input wire                     adc_wEn,
     input wire [ADDRESS_WIDTH-1:0] adc_addr,
-    input wire [DATA_WIDTH-1:0]    adc_dataIn
+    input wire [DATA_WIDTH-1:0]    adc_dataIn,
+
+    // VGA Side (Port C - R)
+    // Note: This port is read-only, no writeback
+    input wire [ADDRESS_WIDTH-1:0] vga_addr,
+    output reg [DATA_WIDTH-1:0]    vga_dataOut = 0
 );
 
 reg [DATA_WIDTH-1:0] MemoryArray[0:DEPTH-1];
@@ -36,6 +42,11 @@ always @(posedge clk) begin
     if(adc_wEn) begin
         MemoryArray[adc_addr] <= adc_dataIn;
     end
+end
+
+// VGA Port (read-only)
+always @(posedge clk) begin
+    vga_dataOut <= MemoryArray[vga_addr];
 end
 
 endmodule
