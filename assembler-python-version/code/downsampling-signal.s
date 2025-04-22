bb271f3dbd151a4c7701c689e6305ab9489e37ec
Wanghley Soares Martins <wanghleys@gmail.com>
2025-04-21 22:30:09 -0400

    fix: update downsampling-signal to improve clarity and correct ECG output handling

--- assembler-python-version/code/downsampling-signal.s
+++ assembler-python-version/code/downsampling-signal.s
@@ -4,21 +4,28 @@ start:
     addi $t7, $zero, 1369     # ECG output base address = 0x559
     addi $t2, $zero, 0        # counter = 0
     addi $t5, $zero, 320      # max count = 320
+    addi $t3, $zero, 2049     # EMG input base address = 0x801
+    # addi $t4, $zero, 0        # ECG input base address = 0x000
+    
 
     # Constants
     addi $t4, $zero, 1000     # EMG constant
-    addi $t6, $zero, 1500     # ECG constant
+    # addi $t6, $zero, 1500     # ECG constant
 
 loop:
     blt  $t2, $t5, do_copy
     j    done
 
 do_copy:
+    # Copy EMG and ECG data
+    lw   $t6, 0($t3)          # read EMG data
+
     sw   $t4, 0($t1)          # write 1000 to EMG output
-    sw   $t6, 0($t7)          # write 1500 to ECG output
+    sw   $t6, 0($t7)          # write ECG data to output
     addi $t1, $t1, 1          # EMG output++
     addi $t7, $t7, 1          # ECG output++
     addi $t2, $t2, 1          # counter++
+    addi $t3, $t3, 1          # EMG input++
     j    loop
 
 done:
