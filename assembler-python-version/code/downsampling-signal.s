19a197748ffb01e71c0197c253614aeea2563e3c
Wanghley Soares Martins <wanghleys@gmail.com>
2025-04-21 21:27:46 -0400

    fix: update downsampling logic to read from input address and store to output

--- assembler-python-version/code/downsampling-signal.s
+++ assembler-python-version/code/downsampling-signal.s
@@ -1,16 +1,19 @@
 start:
+    addi $t0, $zero, 3199     # EMG base address (input) = 0xC7F
     addi $t1, $zero, 1708     # output base address = 0x6AC
     addi $t2, $zero, 0        # counter = 0
+    addi $t3, $zero, 2        # stride = 2
     addi $t5, $zero, 320      # max count = 320
-    addi $t4, $zero, 1234     # constant to write
 
 loop:
     blt  $t2, $t5, do_copy
     j    done
 
 do_copy:
-    sw   $t4, 0($t1)          # store constant to output
-    addi $t1, $t1, 1          # increment address (by word index)
+    lw   $t4, 0($t0)
+    sw   $t4, 0($t1)
+    addi $t0, $t0, 2          # increment input address by 2 words (assuming word addressing)
+    addi $t1, $t1, 1          # increment output address
     addi $t2, $t2, 1          # increment counter
     j    loop
 
