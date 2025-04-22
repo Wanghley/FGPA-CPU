8d80115ed5b2d0e02033fd60fb3fafc1f407b5ed
Wanghley Soares Martins <wanghleys@gmail.com>
2025-04-21 22:36:32 -0400

    fix: correct ECG input address and update data handling in downsampling-signal

--- assembler-python-version/code/downsampling-signal.s
+++ assembler-python-version/code/downsampling-signal.s
@@ -5,11 +5,11 @@ start:
     addi $t2, $zero, 0        # counter = 0
     addi $t5, $zero, 320      # max count = 320
     addi $t3, $zero, 2049     # EMG input base address = 0x801
-    # addi $t4, $zero, 0        # ECG input base address = 0x000
+    addi $t4, $zero, 3199        # ECG input base address = 0xC7F
     
 
     # Constants
-    addi $t4, $zero, 1000     # EMG constant
+    # addi $t4, $zero, 1000     # EMG constant
     # addi $t6, $zero, 1500     # ECG constant
 
 loop:
@@ -19,13 +19,15 @@ loop:
 do_copy:
     # Copy EMG and ECG data
     lw   $t6, 0($t3)          # read EMG data
+    lw   $t0, 0($t4)          # read ECG data
 
-    sw   $t4, 0($t1)          # write 1000 to EMG output
+    sw   $t0, 0($t1)          # write EMG data to output
     sw   $t6, 0($t7)          # write ECG data to output
     addi $t1, $t1, 1          # EMG output++
     addi $t7, $t7, 1          # ECG output++
     addi $t2, $t2, 1          # counter++
     addi $t3, $t3, 1          # EMG input++
+    addi $t4, $t4, 1          # ECG input++
     j    loop
 
 done:
